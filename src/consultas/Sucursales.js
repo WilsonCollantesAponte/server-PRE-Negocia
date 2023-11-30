5e9c574490edf97e3851bd0eeb572a7df0c57f59
AirtonYefri <pache_sagitario28_@hotmail.com>
2023-11-30 13:40:25 -0500

    sucursales

--- src/consultas/Sucursales.js
+++ src/consultas/Sucursales.js
@@ -4,7 +4,7 @@ async function sucursales(req, res) {
   const { empresa, id_usuario, id_empresa } = req.body;
 
   try {
-    const results = await new Promise((resolve, reject) => {
+    const dataSucursal = await new Promise((resolve, reject) => {
       bd_conexion([id_empresa]).query(
         `SELECT almacen_usuario.id_sucursal, almacen.nombre, almacen.direccion, 
         almacen.observaciones FROM almacen, almacen_usuario 
@@ -12,17 +12,17 @@ async function sucursales(req, res) {
         AND (almacen.tipo = '1' OR almacen.tipo='3') AND almacen.empresa = ? AND almacen_usuario.empresa = ?
         GROUP BY almacen_usuario.id_sucursal ORDER BY almacen.id ASC`,
         [id_usuario, empresa, empresa],
-        function (err, results) {
+        function (err, dataSucursal) {
           if (err) {
             reject(err);
           } else {
-            resolve(results);
+            resolve(dataSucursal);
           }
         }
       );
     });
 
-    res.status(200).json(results); // Cambiado de { dataSucursal: results } a solo results
+    res.status(200).json(dataSucursal); // Cambiado de { dataSucursal: results } a solo results
   } catch (error) {
     console.error("Error en la consulta a la base de datos:", error);
     res.status(500).json({
