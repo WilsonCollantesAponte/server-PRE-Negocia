0bcc7cf40546faaf9c0f9bbc71de94022b69865a
AirtonYefri <pache_sagitario28_@hotmail.com>
2023-11-30 10:51:58 -0500

    respuesta - results

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -37,8 +37,7 @@ router.post("/login", async (req, res) => {
     //Responde con informacón que determina si existe un usuario y clave
     res
       .status(200)
-      .json({ userExist: results.length !== 0, passwordIsCorrect }),
-       results[0];
+      .json({ userExist: results.length !== 0, passwordIsCorrect,  userData: results[0] });
   } catch (error) {
     console.error("Error en la consulta a la base de datos:", error);
     res.status(500).json({
