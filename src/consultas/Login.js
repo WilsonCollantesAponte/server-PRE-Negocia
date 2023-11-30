de05d7475f328ff46c176b42d351b931c018a48c
AirtonYefri <pache_sagitario28_@hotmail.com>
2023-11-30 16:48:29 -0500

    mejora - consultas (login)

--- src/consultas/Login.js
+++ src/consultas/Login.js
@@ -47,6 +47,9 @@ async function login(req, res) {
   } finally {
     // Cerrar la conexión a la base de datos aquí
     bd_conexion(1).end();
+    
+    // Asegurarse de que la respuesta se haya enviado antes de finalizar la función
+    res.end();
   }
 }
 
