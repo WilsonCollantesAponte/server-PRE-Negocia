6ecababb97a0da6ef89435415f62593f684625c2
AirtonYefri <pache_sagitario28_@hotmail.com>
2023-11-30 12:18:28 -0500

    mejora - estructura

--- src/consultas/Login.js
+++ src/consultas/Login.js
@@ -1,10 +1,10 @@
 const bcryptjs = require("bcryptjs");
-const bd_conexion = require("../conexion/bd_conexion.js");
+const bd_conexion = require("../conexion/bd_conexion");
 
 async function login(req, res) {
   const { user, clave } = req.body;
-// ...
-try {
+
+  try {
     const results = await new Promise((resolve, reject) => {
       bd_conexion(1).query(
         "SELECT * FROM users WHERE user = ?",
@@ -18,32 +18,27 @@ try {
         }
       );
     });
-  
-    if (results.length === 0) {
-        return res.status(200).json({ userExist: false });
-      }
-      
-      const passwordIsCorrect = await new Promise((resolve, reject) => {
-        bcryptjs.compare(clave, results[0].clave, (err, result) => {
-          if (err) reject(err);
-          else resolve(result);
-        });
+
+    if (results.length === 0) return res.status(200).json({ userExist: false });
+
+    const passwordIsCorrect = await new Promise((resolve, reject) => {
+      bcryptjs.compare(clave, results[0].clave, (err, result) => {
+        if (err) reject(err);
+        else resolve(result);
       });
-      
-      if (passwordIsCorrect) {
-        res.status(200).json({ userExist: true, passwordIsCorrect,  userData: results[0] });
-      } else {
-        res.status(200).json({ userExist: true, passwordIsCorrect: false });
-      }
-      
+    });
+
+    res.status(200).json({
+      userExist: results.length !== 0,
+      passwordIsCorrect,
+      userData: results[0]
+    });
   } catch (error) {
     console.error("Error en la consulta a la base de datos:", error);
     res.status(500).json({
       error: "Hubo un error en la consulta a la base de datos",
     });
   }
-  // ...
-  
 }
 
 module.exports = login;
