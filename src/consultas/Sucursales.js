4d57f27b0a3bce510ebad7b1c26f0494454d4c4c
AirtonYefri <pache_sagitario28_@hotmail.com>
2023-12-07 11:37:31 -0500

    mejora - consultas (sucursal)

--- src/consultas/Sucursales.js
+++ src/consultas/Sucursales.js
@@ -2,34 +2,38 @@ const bd_conexion = require("../conexion/bd_conexion");
 
 async function sucursales(req, res) {
   const { empresa, id_usuario, id_empresa } = req.body;
-  //PONER EN VARIABLE LA CONEXION - CLOSE
-    const con = bd_conexion([id_empresa]);
+
+  // Usar pool de conexiones
+  const pool = bd_conexion.pool([id_empresa]);
+
   try {
     const dataSucursal = await new Promise((resolve, reject) => {
-        con.query(
+      pool.query(
         `SELECT almacen_usuario.id_sucursal, almacen.nombre, almacen.direccion, 
         almacen.observaciones FROM almacen, almacen_usuario 
         WHERE almacen_usuario.id_usuario = ? AND almacen_usuario.id_sucursal = almacen.id AND almacen.estado = 1 
         AND (almacen.tipo = '1' OR almacen.tipo='3') AND almacen.empresa = ? AND almacen_usuario.empresa = ?
         GROUP BY almacen_usuario.id_sucursal ORDER BY almacen.id ASC`,
         [id_usuario, empresa, empresa],
-        function (err, dataSucursal) {
+        function (err, results) {
           if (err) {
             reject(err);
           } else {
-            resolve(dataSucursal);
+            resolve(results);
           }
         }
       );
     });
 
-    res.status(200).json(dataSucursal); // Cambiado de { dataSucursal: results } a solo results
-    con.close(); //cerrar conexion
+    res.status(200).json(dataSucursal);
   } catch (error) {
     console.error("Error en la consulta a la base de datos:", error);
     res.status(500).json({
       error: "Hubo un error en la consulta a la base de datos",
     });
+  } finally {
+    // Liberar la conexión al pool
+    pool.releaseConnection();
   }
 }
 
