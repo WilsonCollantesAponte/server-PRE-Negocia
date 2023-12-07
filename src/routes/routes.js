2d89ff0af5fc014e80c12c148f8535b80ef66a7a
Wilson Collantes Aponte <wilson.aponte000@gmail.com>
2023-12-07 14:19:42 -0500

    add rubros endpoint

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,18 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const controller = require('../controller/controlles');
+const controller = require("../controller/controlles");
+const bd_conexion = require("../conexion/bd_conexion");
 
 // Otras importaciones necesarias
 // Ruta de login
-router.post('/login', controller.login);
+router.post("/login", controller.login);
 
 // Ruta de registro
-router.post('/register', controller.register);
+router.post("/register", controller.register);
 
 // Ruta de sucursales
-router.post('/sucursales', controller.sucursales);
+router.post("/sucursales", controller.sucursales);
 
-
+router.get("/rubros", controller.rubros);
 // Otras rutas, si las tienes
 
 module.exports = router;
