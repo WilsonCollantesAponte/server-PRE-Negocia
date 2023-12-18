5952f653d41fd9434f8d54050e8ddccc2548a2e6
AirtonYefri <pache_sagitario28_@hotmail.com>
2023-12-18 11:57:54 -0500

    consulta - productos

--- src/consultas/ProductosPrincipal.js
+++ src/consultas/ProductosPrincipal.js
@@ -1,26 +1,5 @@
 import { fn_conexion } from ("../conexion/NewConexion.js");
 
-export function explode_filtros_globales_v2(n_campo, n_tipo_campo) {
-var filtro_campo = ''
-
-if (n_tipo_campo) {
-var i = 1
-var or = ''
-
-n_tipo_campo.split(',').forEach(id_valor => {
-var id_campo = parseInt(id_valor.trim())
-
-if (i !== 1) {
-or = ' OR '
-}
-
-filtro_campo += `${or}(${n_campo} = '${id_campo}')`
-i++
-})
-}
-
-return filtro_campo
-}
 
 
 async function fn_principal_producto(req, res) {
