223faa11c29b9c137d9de2b8c42e897ff3db6b9e
AirtonYefri <pache_sagitario28_@hotmail.com>
2023-12-19 12:24:09 -0500

    consulta - productos

--- src/consultas/ProductosDos.js
+++ src/consultas/ProductosDos.js
@@ -3,20 +3,7 @@ const bd_conexion = require("../conexion/bd_conexion");
     
     async function productos_dos(req, res) {
     try {
-    // Log the entire request body
-console.log('Request Body:', req.body);
-
-// Access specific values
-console.log('fn_id_producto:', req.body.fn_id_producto);
-console.log('fn_id_sucursal:', req.body.fn_id_sucursal);
-// ... (other properties)
-
-// Example usage in your code
-var fn_id_producto = req.body.fn_id_producto || '';
-var fn_id_sucursal = parseInt(req.body.fn_id_sucursal) || 0;
-// ... (other variables)
-
-res.status(200).json(req.body);
+res.status(200).json('Request Body');
     } catch (error) {
     return res.status(500).json({ message: 'Algo salió mal: fn_principal_producto' })
     }
